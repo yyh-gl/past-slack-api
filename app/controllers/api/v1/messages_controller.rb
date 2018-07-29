828faf316ffaa662fdfd9d541462278e2bb8a31c
yyh-gl <yhonda.95.gl@gmail.com>
2018-07-29 17:03:19 +0900

    メッセージ取得APIを修正

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::MessagesController < ApplicationController
 
   # 全チャンネルのメッセージを取得（エクスポート）
   def index
-    `#{APP_ROOT_PATH}/lib/slack-dump/slack-dump -h`
+    public = Channel::fetch_channels('public')
+    private = Channel::fetch_channels('private')
+    channels = public['channels'] + private['channels']
+    channels.each do |ch|
+      # `#{APP_ROOT_PATH}/lib/slack-dump/slack-dump #{ch['ch_id']}`
+    end
   end
 
   # 指定チャンネルのメッセージを取得（エクスポート）
