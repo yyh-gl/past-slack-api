56b87a877db6784eb897b5c73d99fcaa9fa1db07
yyh-gl <yhonda.95.gl@gmail.com>
2018-08-01 23:20:04 +0900

    メッセージをDBに保存するリクエスト作成

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -20,4 +20,18 @@ class Api::V1::MessagesController < ApplicationController
 
   end
 
+  # 全チャンネルのメッセージをDBに保存
+  def save_all_messages
+    channels = Dir.glob("#{APP_ROOT_PATH}/log/slack/messages/*")#.map { |ch| File.basename(ch) }
+    channels.each do |ch|
+      Dir.glob("#{ch}/*").each do |json_file|
+        File.open(json_file) do |data|
+          JSON.load(data).each do |hash|
+            puts hash
+          end
+        end
+      end
+    end
+  end
+
 end
